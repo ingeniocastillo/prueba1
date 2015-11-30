93b8fa3ddffd11328f0fc0c9524d8995d66eec73
Nimrod Castillo <ingeniocastillo@icloud.com>
2015-11-30 12:14:52 -0600

    Initial Commit

---
+++ gitPrueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitPrueba
+//
+//  Created by NIMROD CASTILLO ALCÁNTARA on 30/11/15.
+//  Copyright © 2015 NIMROD CASTILLO ALCÁNTARA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
